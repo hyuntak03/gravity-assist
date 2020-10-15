1fe3c6260bdf2ca59073c2db6314d345fad0ff3c
hyuntak03 <hyuntak03.lee@gmail.com>
2020-10-15 15:23:53 +0900

    modified

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Gravity-assist.py is a gravity-assit simulation which is also called gravitiatio
 Gravity-assist.py has low-quality graphics beacuse it is made by turtle graphic.
 <br></br>
 But Gravity-assist.py shows completly same exercise with real gravity-assist effect
+<br></br>
+Gravity-assist.py has two planets(jupiter, saturn)
 #Feature
-Gravity-assist.py shows only accerlation exercise
+Gravity-assist.py shows only acceleration exercise
--- main.py
+++ main.py
@@ -3,27 +3,34 @@ import turtle as z
 import math
 from matplotlib import pyplot as plt
 
-planet = Turtle()
+jupiter = Turtle()
+saturn = Turtle()
 spaceship = Turtle()
 
 launch_angle = math.radians(90)
 
 z.setup(width = 700, height = 700)
-planet.shape("circle")
+jupiter.shape("circle")
+saturn.shape("circle")
 spaceship.shape("circle")
-planet.color("red")
+jupiter.color("red")
+saturn.color("brown")
 spaceship.color("green")
 
 spaceship.shapesize(0.5,0.5)
 
 spaceship.left(90)
-planet.penup()
+jupiter.penup()
 spaceship.penup()
-planet.hideturtle()
+saturn.penup()
+saturn.hideturtle()
+jupiter.hideturtle()
 spaceship.hideturtle()
-planet.goto(-150,0)
-spaceship.goto(-200,-100)
-planet.showturtle()
+jupiter.goto(-150,0)
+spaceship.goto(-170,-100)
+saturn.goto(-90,100)
+saturn.showturtle()
+jupiter.showturtle()
 spaceship.showturtle()
 spaceship.pendown()
 
@@ -34,40 +41,64 @@ v = 0
 velocity = []
 
 while True:
-    distance = math.sqrt((planet.xcor() - spaceship.xcor())**2 + (planet.ycor() - spaceship.ycor())**2)
-    if((planet.xcor() - spaceship.xcor()) != 0):
-        inclination = (planet.ycor() - spaceship.ycor()) / (planet.xcor() - spaceship.xcor())
-    if((planet.xcor() - spaceship.xcor()) == 0):
+    distance = math.sqrt((jupiter.xcor() - spaceship.xcor())**2 + (jupiter.ycor() - spaceship.ycor())**2)
+    distance_saturn = math.sqrt((saturn.xcor() - spaceship.xcor())**2 + (saturn.ycor() - spaceship.ycor())**2)
+
+    if((jupiter.xcor() - spaceship.xcor()) != 0):
+        inclination = (jupiter.ycor() - spaceship.ycor()) / (jupiter.xcor() - spaceship.xcor())
+    if((jupiter.xcor() - spaceship.xcor()) == 0):
         inclination = 0;
 
+    if((saturn.xcor() - spaceship.xcor()) != 0):
+        inclination_saturn = (jupiter.ycor() - spaceship.ycor()) / (jupiter.xcor() - spaceship.xcor())
+    if((saturn.xcor() - spaceship.xcor()) == 0):
+        inclination_saturn = 0;
+
+    angle_saturn = math.atan(inclination_saturn)
     angle = math.atan(inclination)
     # gravity = G * ((1.898 * 10**27) * 5000) / (distance)**2
     gravity = 300 / (distance)**2
+    gravity_saturn = 300 / (distance_saturn)**2
     #This gravity is not accurate figure
 
-    if(spaceship.xcor() < planet.xcor() and spaceship.ycor() <= planet.ycor()):
+    if(spaceship.xcor() < jupiter.xcor() and spaceship.ycor() <= jupiter.ycor()):
         v_x = v_x + gravity * math.cos(angle)
         v_y = v_y + gravity * math.sin(angle)
-    if(spaceship.xcor() <= planet.xcor() and spaceship.ycor() > planet.ycor()):
+    if(spaceship.xcor() <= jupiter.xcor() and spaceship.ycor() > jupiter.ycor()):
         v_x = v_x + gravity * math.cos(angle)
         v_y = v_y - gravity * math.sin(angle)
-    if(spaceship.xcor() > planet.xcor() and spaceship.ycor() >= planet.ycor()):
+    if(spaceship.xcor() > jupiter.xcor() and spaceship.ycor() >= jupiter.ycor()):
         v_x = v_x - gravity * math.cos(angle)
         v_y = v_y - gravity * math.sin(angle)
-    if(spaceship.xcor() > planet.xcor() and spaceship.ycor() >= planet.ycor()):
+    if(spaceship.xcor() > jupiter.xcor() and spaceship.ycor() >= jupiter.ycor()):
         v_x = v_x - gravity * math.cos(angle)
         v_y = v_y + gravity * math.sin(angle)
+
+    if(spaceship.xcor() < saturn.xcor() and spaceship.ycor() < saturn.ycor()):
+        v_x = v_x + gravity_saturn * math.sin(angle_saturn)
+        v_y = v_y + gravity_saturn * math.sin(angle_saturn)
+    if(spaceship.xcor() <= saturn.xcor() and spaceship.ycor() > saturn.ycor()):
+        v_x = v_x + gravity_saturn * math.cos(angle_saturn)
+        v_y = v_y - gravity_saturn * math.sin(angle_saturn)
+    if(spaceship.xcor() > saturn.xcor() and spaceship.ycor() >= saturn.ycor()):
+        v_x = v_x - gravity_saturn * math.cos(angle_saturn)
+        v_y = v_y - gravity_saturn * math.sin(angle_saturn)
+    if(spaceship.xcor() > jupiter.xcor() and spaceship.ycor() >= jupiter.ycor()):
+        v_x = v_x - gravity_saturn * math.cos(angle_saturn)
+        v_y = v_y + gravity_saturn * math.sin(angle_saturn)
 
     dx = spaceship.xcor() + v_x
     dy = spaceship.ycor() + v_y
     seta = math.atan(v_y / v_x)
     v = abs(v_x / math.cos(seta))
     velocity.append(v)
-    planet.forward(1)
+    jupiter.forward(1)
+    saturn.forward(1)
     spaceship.goto(dx,dy)
-    if(planet.xcor() == 350.0):
+    if(jupiter.xcor() == -50.0):
         break;
 
+plt.title("Velocity of Space Ship")
 plt.plot(velocity)
 plt.show()
 z.done()
